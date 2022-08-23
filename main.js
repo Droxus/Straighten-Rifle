ccc5c55db0c59c6ee2d5762f7f84d5b941b19be4
Droxus <alexredov@gmail.com>
2022-08-23 15:30:09 +0300

    +green cube; start settings

--- main.js
+++ main.js
@@ -1,1 +1,42 @@
-console.log('Yeah')
+
+import * as THREE from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/r99/three.module.js';
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+let WIDTH = window.innerWidth;
+let HEIGHT = window.innerHeight;
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+animate();
+
+cube.rotation.x += 0.01;
+cube.rotation.y += 0.01;
+
+function animate() {
+    requestAnimationFrame( animate );
+
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+
+    renderer.render( scene, camera );
+};
+
+window.addEventListener('resize', onResize)
+document.oncontextmenu = document.body.oncontextmenu = function() {return false;}
+
+function onResize(){
+    console.log()
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+}
